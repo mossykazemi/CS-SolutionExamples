dacbfb420fdc6ac373db09a691794112801beda2
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-08 08:56:05 +0330

    sulotion one commit

---
+++ Solution1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number between 1 to 10");
+            var number = Convert.ToInt32(Console.ReadLine());
+            if (number >= 1 && number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+                Console.WriteLine("Invalid");
+        }
+    }
+}
