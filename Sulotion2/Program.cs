a6e836f27c9dc748e2dc4c3cca251f859d2a135f
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-08 09:02:12 +0330

    commited sulotion 2

---
+++ Sulotion2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number:");
+            var num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number:");
+            var num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 < num2)
+                Console.WriteLine("{0} is Bigger than {1}", num2, num1);
+            else if (num2 < num1)
+                Console.WriteLine("{0} is Bigger than {1}", num1, num2);
+            else if (num1 == num2)
+                Console.WriteLine("Numbers are equal");
+        }
+    }
+}
