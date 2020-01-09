55f097024c5fa2bee4ec939329414129f02a5d15
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-10 02:23:04 +0330

    commit soution7

---
+++ Solution7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Solution7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, number, factorial = 1;
+            Console.WriteLine("Enter a number");
+            number = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine(number + "!" + " =" + factorial);
+        }
+    }
+}
