e9be6415d8e8744b6164b4fb7aec6f3995c7e117
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-09 03:45:51 +0330

    commit solution3

---
+++ Solution3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the image Width (cm):");
+            var width = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the image Height (cm):");
+            var height = Convert.ToDouble(Console.ReadLine());
+            if (width > height)
+            {
+                Console.WriteLine("image is Landscape");
+            }
+            else if (height > width)
+            {
+                Console.WriteLine("image is Portrait");
+            }
+            else
+            {
+                Console.WriteLine("image is Square");
+            }
+        }
+    }
+}
