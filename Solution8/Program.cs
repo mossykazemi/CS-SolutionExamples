a14a8dc022458f431911872d0e279b507102a045
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-12 02:27:57 +0330

    Solution8 commited

---
+++ Solution8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int randNum = random.Next(1, 10);
+            Console.WriteLine(randNum);
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Guess the random number :");
+                var guess = Convert.ToInt32(Console.ReadLine());
+                if (guess == randNum)
+                {
+                    Console.WriteLine("You win!");
+                    break;
+                }
+                else if (i == 4)
+                    Console.WriteLine("You Lost!");
+                else
+                    continue;
+
+            }
+        }
+    }
+}
