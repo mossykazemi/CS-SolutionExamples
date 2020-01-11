e7d4aaf04cf3b45e4cc5d7766510669cb14e2c06
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-11 06:21:54 +0330

    Commit 5

---
+++ Solution5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution5
+{
+    //Finding Divisible numbers by 3 betwen 1 and 100
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                    Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
