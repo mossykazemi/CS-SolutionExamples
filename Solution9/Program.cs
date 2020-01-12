f1d3206ef1b194088af6ef8f03f1ca9466343967
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-13 03:02:00 +0330

    solution 9 commited

---
+++ Solution9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = new List<string>();
+            var input = "";
+            var exit = String.IsNullOrEmpty(input);
+            do
+            {
+                input = Console.ReadLine();
+                numbers.Add(input);
+            }
+            while (!exit);
+            if (exit)
+            {
+
+                List<int> numsum = input.Split(',').Select(int.Parse).ToList();
+
+                Console.WriteLine("minimum number is : " + numsum.Min());
+            }
+        }
+    }
+}
