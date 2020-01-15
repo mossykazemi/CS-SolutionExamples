fdd149a16c895c0d6d024e4415c310f397a35faf
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-15 17:24:14 +0330

    commit 11

---
+++ Solution11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution11
+{
+    class Program
+    {
+        public static string Reverse(string name)
+        {
+            char[] charArray = name.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+            {
+                array[name.Length - i] = name[i - 1];
+            }
+            return= new string(array);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine(Reverse(name));
+            Console.WriteLine(ReverseName(name));
+            Console.ReadKey();
+        }
+    }
+}
