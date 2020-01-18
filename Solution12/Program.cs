a2178cab4d2938416ac77e9cf2f7c2d2b60f1ee2
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-18 23:28:02 +0330

    Commit solution 12

---
+++ Solution12/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution12
+{
+    class Program
+    {
+        public static bool ContainsDuplicates(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[i] == a[j]) return true;
+                }
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter five numbers: ");
+            var numbers = new int[5];
+            numbers[0] = Convert.ToInt32(Console.ReadLine());
+            numbers[1] = Convert.ToInt32(Console.ReadLine());
+            numbers[2] = Convert.ToInt32(Console.ReadLine());
+            numbers[3] = Convert.ToInt32(Console.ReadLine());
+            numbers[4] = Convert.ToInt32(Console.ReadLine());
+
+            ContainsDuplicates(numbers);
+            if (ContainsDuplicates(numbers) == true)
+            {
+                Console.WriteLine("try again");
+            }
+            else
+            {
+                Array.Sort(numbers);
+                foreach (int x in numbers)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+        }
+    }
+}
