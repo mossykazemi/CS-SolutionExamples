5ee32659b92b4c0cabd6c062a28beed15cba5afc
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-20 15:10:33 +0330

    commited 15

---
+++ Solution15/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some numbers seperated by hyphen (-):");
+            var input = Console.ReadLine();
+            var numArray = input.Split('-');
+            int firstValue = Convert.ToInt32(numArray[0]);
+            bool Cons = true;
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                if (Convert.ToInt32(numArray[i]) != firstValue)
+                    Cons = false;
+                break;
+            }
+            var result = Cons ? "Consecutive" : "Not Consecutive";
+            Console.WriteLine(result);
+
+
+            
+
+            //var numbers = new List<int>();
+            //Console.WriteLine("Type a hypen separated numbers list");
+            //var input = Console.ReadLine();
+            //var numberList = input.Split('-');
+            //foreach (var i in numberList)
+            //{
+            //    numbers.Add(Convert.ToInt32(i));
+            //}
+            //var isConsecutive = true;
+            //numbers.Sort();
+            //for (var i = 0; i < numbers.Count - 1; i++)
+            //{
+            //    if (numbers[i + 1] != numbers[i] + 1)
+            //    {
+            //        isConsecutive = false;
+            //        break;
+            //    }
+            //}
+            //var result = (isConsecutive) ? "Consecutive" : "Non consecutive";
+            //Console.WriteLine(result);
+        }
+    }
+}
