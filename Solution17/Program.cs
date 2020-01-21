5e8315f25ff29a2796fbd8277be9e9030e1ef3ac
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-22 02:40:07 +0330

    Commited 17

---
+++ Solution17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the time value in 24-hour format :");
+            var input = Console.ReadLine();
+            var time = input.Split(':');
+            var hour = Convert.ToInt32(time[0]);
+            var minute = Convert.ToInt32(time[1]);
+            var IsValid = true;
+            if (String.IsNullOrWhiteSpace(input))
+                IsValid = false;
+            else if (hour > 23 || hour < 00 || minute > 59 || minute < 00)
+                IsValid = false;
+            else
+                IsValid = true;
+
+            var result = (IsValid) ? "Valid" : "Not Valid";
+            Console.WriteLine(result);
+        }
+    }
+}
