460f0b4b5f78df16346a0d1c974c5c1c047743ce
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-21 15:16:42 +0330

    commited 17

---
+++ Solution17/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numList = new List<int>();
+            Console.WriteLine("Enter some numbers Sperated by hyphen :");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(input))
+                Console.WriteLine("bye");
+            else
+            {
+                var numbers = input.Split('-');
+                foreach (var i in numbers)
+                    numList.Add(Convert.ToInt32(i));
+                numList.Sort();
+                var isDuplicate = true;
+                if (numList.Distinct().Count() != numList.Count)
+                    isDuplicate = true;
+                else
+                    isDuplicate = false;
+                Console.WriteLine(isDuplicate ? "Duplicate" : "Not Duplicate");
+            }
+        }
+    }
+}
