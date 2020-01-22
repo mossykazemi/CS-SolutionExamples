e400ac89a2629d3584bc2d9895d317c9fdfddbb5
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-23 02:44:28 +0330

    Commited 18

---
+++ Solution18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type your sentence :");
+            var input = Console.ReadLine();
+            var words = input.ToLower().Split(' ');
+            var result = "";
+            for (var i = 0; i < words.Length; i++)
+            {
+                result += words[i][0].ToString().ToUpper() + words[i].Substring(1) + " ";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
