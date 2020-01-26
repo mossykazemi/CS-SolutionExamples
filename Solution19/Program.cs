5e72c871e54993343aaad955437878eda118accd
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-26 18:24:45 +0330

    Commited 19

---
+++ Solution19/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an English word :");
+            var input = Console.ReadLine();
+
+            //vowels are 'a', 'e', 'o', 'u', 'i'
+            var vowelList = new List<char> { 'a', 'e', 'o', 'u', 'i' };
+            var vowels = 0;
+
+            foreach (var letter in input.ToLower())
+            {
+                if (vowelList.Contains(letter))
+                    vowels++;// vowels counter 
+            }
+
+            Console.WriteLine("There are {0} Vowels in {1}", vowels, input);
+            Console.ReadKey();
+        }
+    }
+}
