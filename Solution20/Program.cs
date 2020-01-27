5083900cbe8a3855f94eac6d8099e22d37cc04cb
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-27 04:57:26 +0330

    Cmmited 20

---
+++ Solution20/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Solution20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\mkaze\Desktop\Secret.txt";
+            var text = File.ReadAllText(path);
+            var word = text.Split(' ');
+            Console.WriteLine(word.Length);
+        }
+    }
+}
