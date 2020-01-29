2751bb294cf5b780701000959f43535e0c2bde87
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-01-29 05:09:50 +0330

    cmed21

--- Solution21/Program.cs
+++ Solution21/Program.cs
@@ -14,9 +14,11 @@ namespace Solution21
         {
             //Create secret.txt file on your desktop and put some words in it
             var path = @"C:\Users\yourName\Desktop\Secret.txt";
-            // 
+            //Read all the text and make it lowercase
             var text = File.ReadAllText(path).ToLower();
+            //Using regex
             var match = Regex.Match(text, "\\w+");
+
             Dictionary<string, int> freq = new Dictionary<string, int>();
             while (match.Success)
             {
